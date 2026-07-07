d9972af0b476025a55a69c202f70abac397bcfe2
Divyansh Dev <devdivyansh23@gmail.com>
2026-07-07 23:26:10 +0530

    Configure env variables and stripe SDK

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const stripe =require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
+app.use(express.json());
 app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log("Stripe SDK initialized");
 });
